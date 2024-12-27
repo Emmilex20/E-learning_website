66b29a1b2b36b320a6b61edb2ca45597d232ee80
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2024-12-27 04:31:40 +0100

    Initial commit

---
+++ constant/constant.ts
@@ -0,0 +1,51 @@
+export const navLinks = [
+    {
+      id: 1,
+      url: "#home", // Updated URL
+      label: "Home",
+      icon: null,
+      target: "_self",
+      ariaLabel: "Navigate to Home",
+    },
+    {
+      id: 2,
+      url: "#about",
+      label: "About",
+      icon: null,
+      target: "_self",
+      ariaLabel: "Learn more about us",
+    },
+    {
+      id: 3,
+      url: "#courses",
+      label: "Courses",
+      icon: null,
+      target: "_self",
+      ariaLabel: "Explore our courses",
+    },
+    {
+      id: 4,
+      url: "#testimonials",
+      label: "Testimonial",
+      icon: null,
+      target: "_self",
+      ariaLabel: "See what our clients say",
+    },
+    {
+      id: 5,
+      url: "#blog",
+      label: "Blog",
+      icon: null,
+      target: "_self",
+      ariaLabel: "Read our latest articles",
+    },
+    {
+      id: 6,
+      url: "#contact",
+      label: "Contact",
+      icon: null,
+      target: "_self",
+      ariaLabel: "Get in touch with us",
+    },
+  ];
+  
